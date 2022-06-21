41e6a240c362fa2f79f631e4398fe8773b129eeb
papasmf1 <papasmf1@gmail.com>
2022-06-21 11:37:07 +0900

    4장 완료

--- DemoLoop.py
+++ DemoLoop.py
@@ -45,3 +45,22 @@ print([i**2 for i in lst if i > 5])
 tp = ("apple", "orange", "kiwi")
 print([len(i) for i in tp])
 
+#필터링 하는 함수
+lst = [10, 25, 30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+print("---필터링---")
+def getBiggerThan20(i):
+    return i > 20
+
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+print("---람다함수---")
+iterL = filter(lambda i:i>20, lst)
+for item in iterL:
+    print(item)
+

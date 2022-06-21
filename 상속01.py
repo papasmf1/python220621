5a132eecf5424ccd9a5cf67ada7112f7ff2afb2e
papasmf1 <papasmf1@gmail.com>
2022-06-21 15:45:14 +0900

    클래스 연습 완료

--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,35 @@
+#부모 클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
-
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+    def methodA(self):
+        print("부모쪽 코드 실행")
+#자식 클래스 정의
 class Student(Person):
+    #상속받은 메서드를 덮어쓰기(재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 생성자 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #상속받은 메서드를 재정의 
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(
+            self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
-
+s = Student("이순신", "010-111-1234", "컴공", "190101")
+#object클래스에 정의된 스페셜 멤버 변수 
+# print(p.__dict__)
+# print(s.__dict__)
+p.printInfo()
+s.printInfo() 
+p.methodA()
+s.methodA()
 
